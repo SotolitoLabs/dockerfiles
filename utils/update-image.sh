def30b294742681ad83dfcc53fe799fb6d1a5d45
Ivan Chavero <ichavero@redhat.com>
2015-10-11 13:53:13 -0500

    Update image names

--- utils/update-image.sh
+++ utils/update-image.sh
@@ -3,10 +3,10 @@
 #Updates the Fedora 22 image 
 
 docker rm fedora-22-base-x86_64
-docker run --name=fedora-22-base-x86_64 -i imcsk8/fedora-22-base-x86_64:22 dnf update -y
-docker commit -m 'Daily Update' fedora-22-base-test sotolitolabs/fedora-base:22
+docker run --name=fedora-22-server-x86_64 -a STDOUT -a STDERR imcsk8/fedora-22-server-x86_64:22 dnf update -y
+docker commit -m 'Daily Update' fedora-22-server-x86_64 sotolitolabs/fedora-22-server-x86_64:22
 #Nedd the credentials stored in .dockercfg
 docker login -u imcsk8
-docker push imcsk8/fedora-22-base-x86_64
+docker push imcsk8/fedora-22-server-x86_64
 
 
